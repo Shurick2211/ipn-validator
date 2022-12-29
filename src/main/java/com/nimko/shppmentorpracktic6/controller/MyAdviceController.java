205c4ef65826a6ac927e541a729e3db889fac4a3
sn <shurick2211@gmail.com>
2022-12-29 19:50:06 +0200

    done +

--- src/main/java/com/nimko/shppmentorpracktic6/controller/MyAdviceController.java
+++ src/main/java/com/nimko/shppmentorpracktic6/controller/MyAdviceController.java
@@ -3,20 +3,16 @@ package com.nimko.shppmentorpracktic6.controller;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class MyAdviceController {
-
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -25,8 +21,7 @@ public class MyAdviceController {
 
         return ResponseEntity
                 .badRequest()
-                .body(
-                        ex.getBindingResult().getAllErrors().stream()
+                .body(ex.getBindingResult().getAllErrors().stream()
                                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                                 .collect(Collectors.toList()).toString());
     }
